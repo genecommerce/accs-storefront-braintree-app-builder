d49221bddc2d8a558ad1a6b8e812745c15dd1c46
Craig Newbury <craig@gene.co.uk>
2025-07-09 12:32:02 +0100

    Update drop-in, removed code thats auto-installed on deployment

--- scripts/__dropins__/dropin-braintree/src/helpers/getCart.js
+++
@@ -1,8 +0,0 @@
-import { events } from '@dropins/tools/event-bus.js';
-
-/**
- * @returns {import('@dropins/storefront-cart/data/models').CartModel}
- */
-export default () => (
-  events.lastPayload('cart/data') || events.lastPayload('cart/initialized')
-);
